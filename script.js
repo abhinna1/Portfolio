bc40585628c84aab23812d58ab661c597d5c3b97
abhinna1 <manandharabhinna@gmail.com>
2022-08-19 21:57:00 +0545

    section-1 made responsive and gsap animations added.

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-gsap.to('.nav-title',{
-    position: "static",
-    // scrollTrigger: {
-    //     trigger: '.nav-title',
-    //     start: 'top center',
-    // }
-})
+gsap.fromTo('.img-container', {opacity: -1, x: 100}, {opacity: 1, x:0, duration:0.5})
+
+gsap.fromTo('.portfolio-detail', {opacity: -1, x: -100}, {opacity: 1, x:0, duration:1.5})
