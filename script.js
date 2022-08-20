b8765fe70983bb57cf3feed0a54de450954057e6
abhinna1 <manandharabhinna@gmail.com>
2022-08-20 14:13:11 +0545

    animation finishing

--- script.js
+++ script.js
@@ -1,3 +1,45 @@
-gsap.fromTo('.img-container', {opacity: -1, x: 100}, {opacity: 1, x:0, duration:0.5})
+gsap.registerPlugin('ScrollTrigger');
 
-gsap.fromTo('.portfolio-detail', {opacity: -1, x: -100}, {opacity: 1, x:0, duration:1.5})
+gsap.fromTo('.img-container', {opacity: -1}, {opacity: 1, duration:1});
+
+gsap.fromTo('.portfolio-detail', {opacity: -1, x: -100}, {opacity: 1, x:350, duration:2});
+
+
+
+// gsap.from('.img-container', {})
+gsap.to('.img-container',{
+    scrollTrigger:{
+        scrub: 1,
+        pin: '.img-container',
+        start: 0,
+        end: 1200,
+    },
+    x:200,
+    // ducation: 3,
+});
+
+gsap.to('.navbar',{
+    scrollTrigger:{
+        scrub: 1,
+        start: 0,
+        end: 2,
+    },
+    backgroundColor: '#2B4865',
+    // ducation: 3,
+});
+
+gsap.from('.portfolio-detail', {opacity: 1, x:350})
+gsap.to('.portfolio-detail',{
+    scrollTrigger:{
+        scrub: true,
+        // pin: '.portfolio-detail',
+        start: 0,
+        end: '+=300',
+        trigger: '.portfolio-detail',
+        onUpdate: (e)=>{console.log(e)}
+    },
+    x: 0,
+    opacity: 0,
+});
+
+// gsap.to('.img-container', {scrollTrigger: {scrub: 1},top:'-50px', stagger: .5, duration: 3})
